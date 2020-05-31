c6b707380059290c9c303bdb49fc08a4abf63e82
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-05-31 19:32:21 +0700

    add infra stuff

--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post('/posts', async (req, res) => {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type : 'PostCreated',
         data : {
             id , title
@@ -38,5 +38,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+    console.log('V61');
     console.log('Listening on port 4000');
 });
