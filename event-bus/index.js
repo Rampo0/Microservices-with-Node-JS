f0e2cbf418555a78482b12d3cd33d8d523d68965
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-06-01 14:18:28 +0700

    complete infra

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,9 +14,9 @@ app.post('/events', (req , res) => {
     events.push(event);
 
     axios.post('http://posts-clusterip-srv:4000/events' , event);
-    // axios.post('http://localhost:4001/events' , event);
-    // axios.post('http://localhost:4002/events' , event);
-    // axios.post('http://localhost:4003/events' , event);
+    axios.post('http://comments-clusterip-srv:4001/events' , event);
+    axios.post('http://query-clusterip-srv:4002/events' , event);
+    axios.post('http://moderation-clusterip-srv:4003/events' , event);
 
     res.send({ status : 'OK'});
 });
